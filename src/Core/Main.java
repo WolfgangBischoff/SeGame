0703a324c993b2f25a0fed0afaf4c09603911693
wbischof <wolfgang.bischoff@outlook.at>
2019-08-27 09:54:05 +0200

    Added Console Title, Refactoring

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -8,18 +8,19 @@ import javafx.stage.Stage;
 public class Main extends Application
 {
 // https://stackoverflow.com/questions/33494052/javafx-redirect-console-output-to-textarea-that-is-created-in-scenebuilder
-    public static void main(String[] args)  throws IOException
+    public static void main(String[] args)
     {
         launch(args);
     }
 
     @Override
-    public void start(Stage gameWindow) throws Exception
+    public void start(Stage gameWindow)
     {
         Simulation simulation = new Simulation();
 
         //Console Window
         Stage consoleWindow = new Stage();
+        consoleWindow.setTitle("SeGame Console");
         consoleWindow.setScene(new Scene(simulation.getInterpreter().getConsole()));
         consoleWindow.show();
 
